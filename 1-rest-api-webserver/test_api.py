c89285ebbeac8bf88c2c80a11821442806a2a6fa
Dawid Bozek <dawidb11@gmail.com>
2025-11-08 12:52:08 +0100

    Finishing the 1st exercise

--- 1-rest-api-webserver/test_api.py
+++ 1-rest-api-webserver/test_api.py
@@ -1,11 +1,8 @@
 import os
 os.environ['DATABASE_URI'] = 'sqlite:///:memory:'
-
-
 import pytest
 from app import app, db
 from models import StudentModel
-
 
 @pytest.fixture
 def test_client():
@@ -45,8 +42,40 @@ def test_show_all(test_client):
     assert len(data) == 1
     assert data[0]['name'] == 'Jan Kowalski'
 
-# def test_return_id(test_client):
-    
-# def test_update_data(test_client):
+def test_return_id(test_client):
+    with app.app_context():
+        student = StudentModel(student_id=1, name='Jan Kowalski', age=23, field='Journalism')
+        db.session.add(student)
+        db.session.commit()
+    response = test_client.get('/api/v1/data/1', headers={
+        'Accept': 'application/json'
+    })
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['id'] == 1
+  
+def test_update_data(test_client):
+    with app.app_context():
+        student = StudentModel(student_id=1, name='Jan Kowalski', age=23, field='Journalism')
+        db.session.add(student)
+        db.session.commit()
+    response = test_client.post('/api/v1/data/1/update', json={
+        'name': 'Jan Kowalski',
+        'age': 23,
+        'field': 'IT'
+    }, follow_redirects=True)
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data['field'] == 'IT'
 
-# def test_delete_data(test_client):
+def test_delete_data(test_client):
+    with app.app_context():
+        student = StudentModel(student_id=1, name='Jan Kowalski', age=23, field='Journalism')
+        db.session.add(student)
+        db.session.commit()
+    response = test_client.post('/api/v1/data/1/delete', headers={
+        'Accept': 'application/json'
+    })
+    data = response.get_json()
+    assert data['info']['message'] == 'Student with id 1 is deleted.'
+    assert not data['students_list']
